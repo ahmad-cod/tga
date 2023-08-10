5e6e2a5eaddd127e26913bddd5acf2403a1691fd
ahmad-cod <ahmadadewale800@gmail.com>
2023-08-10 22:36:09 +0100

    newLesson

---
+++ app.js
@@ -0,0 +1,24 @@
+let firstName = "ADAM j";
+let sentence = "Adam said: \"he will be a top notch software engineer\" is new"
+let lastName = "Wale"
+let fullName = `${firstName} ${lastName}`;
+let verb = sentence.slice(5, 9)
+let modifiedFirstName = firstName.replaceAll('A', 'E')
+let splittedSentence = sentence.split(' ')
+let smallFirstName = firstName.toLowerCase();
+
+// const browserType = "mozilla";
+
+// if (browserType.startsWith("zilla")) {
+//   console.log("Found zilla!");
+// } else {
+//   console.log("No zilla here!");
+// }
+
+// console.log('id index', sentence.indexOf('id'));
+
+console.log(splittedSentence)
+
+// console.log('full name: ', fullName)
+// console.log('smallfirstname', smallFirstName)
+// console.log(sentence.length);
