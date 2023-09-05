9438d5da8c5d44b6bc13ff4ebed98c8e989fa45a
ahmad-cod <ahmadadewale800@gmail.com>
2023-09-05 14:10:56 +0100

    newLesson

---
+++ function.js
@@ -0,0 +1,39 @@
+function sayHello(name) {
+  console.log('Hello ' + name);
+}
+
+function sum(num1, num2) {
+  return `sum of ${num1} and ${num2} = ${num1 + num2}`;
+}
+
+function multiply(c, d) {
+  return c * d;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+// sum(100, 200)
+
+const result = sum(100, 100)
+
+console.log(result)
+//
+// ajflakmf;al
+
+sum(23, 24);
+//aa;jga;a
+
+//aa;jga;a
+// aapgja
+// ajflakmf;al
+//aa;jga;a
+// aapgja
+// ajflakmf;al
+//aa;jga;a
+// aapgja
